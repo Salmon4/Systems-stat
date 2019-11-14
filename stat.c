bcef3d2b5edc0dafb7507ad141cc717b6250c4a4
Brandon <bchen00@stuy.edu>
2019-11-14 01:52:09 +0000

    got basic functions working

--- stat.c
+++ stat.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+
 int main(){
-  stat()
+  struct stat buffer;
+  stat("README.md", &buffer);
+  printf("The file size of %s is %d", "README.md", buffer.st_size);
+  printf("\n");
+  printf("The file permission of %s in octal is %d", "README.md", buffer.st_mode);
+  printf("\n");
+  printf("The file %s was last accessed on %s", "README.md", ctime(&buffer.st_atime));
   return 1;
 }
